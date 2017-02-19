4feda50f89d06cc02c277d42662ffdb52a0a41f8
bobozhang <emilyzhangbo@163.com>
2017-02-19 22:06:43 +0800

    commit

--- src/com/bo/javawebmvc/Student.java
+++ src/com/bo/javawebmvc/Student.java
@@ -9,5 +9,26 @@ package com.bo.javawebmvc;
  * 
  */
 public class Student {
-
+	private Integer id;
+	private String studentName;
+	private String gender;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
 }
