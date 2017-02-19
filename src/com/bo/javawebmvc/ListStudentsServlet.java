1489381077e0fbfe69deb2f001063aeacf4c9826
bobozhang <emilyzhangbo@163.com>
2017-02-19 22:02:13 +0800

    代码提交信息

---
+++ src/com/bo/javawebmvc/ListStudentsServlet.java
@@ -0,0 +1,33 @@
+package com.bo.javawebmvc;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ListStudentsServlet
+ */
+public class ListStudentsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ListStudentsServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setAttribute("students", Arrays.asList("aa","bb","cc"));
+		request.getRequestDispatcher("/students.jsp").forward(request, response);
+	}
+
+}
---
+++ src/com/bo/javawebmvc/Student.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.bo.javawebmvc;
+
+/**
+ * @author zhangbo
+ * 2017年2月19日  下午9:59:10
+ * 
+ */
+public class Student {
+
+}
